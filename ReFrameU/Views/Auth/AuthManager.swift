a1ef5f089a0bc18102b58e4f6ddb618ec99717d2
nysajain <147955985+nysajain@users.noreply.github.com>
2025-04-06 23:46:23 -0700

    Update AuthManager.swift

--- ReFrameU/Views/Auth/AuthManager.swift
+++ ReFrameU/Views/Auth/AuthManager.swift
@@ -1,12 +1,3 @@
-//
-//  AuthManager.swift
-//  ReFrameU
-//
-//  Created by Nysa Jain on 06/04/25.
-//
-
-
-import Foundation
 import FirebaseAuth
 
 class AuthManager {
@@ -14,7 +5,7 @@ class AuthManager {
 
     private init() {}
 
-    func signUp(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+    func signUp(name: String, email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
         Auth.auth().createUser(withEmail: email, password: password) { result, error in
             if let error = error {
                 completion(.failure(error))
@@ -24,7 +15,7 @@ class AuthManager {
         }
     }
 
-    func login(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
+    func signIn(email: String, password: String, completion: @escaping (Result<Void, Error>) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if let error = error {
                 completion(.failure(error))
@@ -40,9 +31,5 @@ class AuthManager {
 
     func isLoggedIn() -> Bool {
         return Auth.auth().currentUser != nil
-    }
-
-    func currentUserUID() -> String? {
-        return Auth.auth().currentUser?.uid
     }
 }
